18bac9979a8220843eea6f2b7d6374f1d0653ddc
Filipe Morais <fi.morais.mail@gmail.com>
2018-08-21 14:52:54 -0300

    Creates an delete comments routes.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -147,4 +147,69 @@ router.post(
     });
   }
 );
+
+// @route POST api/posts/comment/:id
+// @desc Add Comment to post
+// @access Private route
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+        post.save().then(post => res.json(post));
+      })
+      .catch(errors =>
+        res.status(404).json({ postnotfound: 'No post was found.' })
+      );
+  }
+);
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Remove comment from post
+// @access Private route
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        // check if comment exists
+        if (
+          post.comments.filter(
+            comment => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: 'This comment does not exist.' });
+        }
+
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        post.comments.splice(removeIndex, 1);
+        post.save().then(post => res.json(post));
+      })
+      .catch(errors =>
+        res.status(404).json({ postnotfound: 'No post was found.' })
+      );
+  }
+);
+
 module.exports = router;
