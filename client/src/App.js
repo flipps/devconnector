3c5c586773073a830a36f7e422057f3ec545ccf2
Filipe Morais <fi.morais.mail@gmail.com>
2018-08-22 10:45:12 -0300

    Basic home layout.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+//components
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Landing from './components/layout/Landing';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <Navbar />
+        <Landing />
+        <Footer />
       </div>
     );
   }
